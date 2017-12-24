78da181386f5d87a9b0bb199bb57a16772e078ba
tzattack <tzattack@gmail.com>
2017-12-24 16:20:39 +0800

    add base q learning frame

---
+++ algos/value_based/q_learning.py
@@ -0,0 +1,19 @@
+import gym
+import numpy as np
+import tensorflow as tf
+from algos.base import RLAlgorithm
+from algos.agent import Agent
+from algos.train_agent import trainAgent
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+class QLearningAgent(Agent):
+    pass
+
+class QLearning(RLAlgorithm):
+    def __init__():
+        pass
+
+    def train(self):
+        pass
